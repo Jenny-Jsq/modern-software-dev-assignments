a06828bf0cd4066fb89137df6d79354151462ab6
Jenny-Jsq <jennymdvs24@gmail.com>
2026-02-27 02:31:39 +0100

    Update self_consistency_prompting.py

--- week1/self_consistency_prompting.py
+++ week1/self_consistency_prompting.py
@@ -9,7 +9,7 @@ load_dotenv()
 NUM_RUNS_TIMES = 5
 
 # TODO: Fill this in! Try to get as close to 100% correctness across all runs as possible.
-YOUR_SYSTEM_PROMPT = ""
+YOUR_SYSTEM_PROMPT = "Reconduct the USER_PROMPT for 5 times, and take the most common answer as the result. Only output the result in the format of "Answer: <number>""
 
 USER_PROMPT = """
 Solve this problem, then give the final answer on the last line as "Answer: <number>".
